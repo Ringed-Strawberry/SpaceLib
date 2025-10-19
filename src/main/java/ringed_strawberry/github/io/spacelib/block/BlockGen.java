841ef0d6eb9cd6e702472da2c12a1e98e8b10718
saturn <thecolorizedsystem@gmail.com>
2025-10-19 20:10:25 +0300

    Improved Block & Item gen

--- src/main/java/ringed_strawberry/github/io/spacelib/block/BlockGen.java
+++ src/main/java/ringed_strawberry/github/io/spacelib/block/BlockGen.java
@@ -1,20 +1,33 @@
 package ringed_strawberry.github.io.spacelib.block;
 
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
+
 public class BlockGen {
-    public static Item createBlockItem(Block block, String name, String MOD_ID){
-        return Registry.register(Registries.ITEM, Identifier.of(MOD_ID, name),
-                new BlockItem(block, new Item.Settings()));
+    public static RegistryKey<Block> keyOf(Identifier id) {
+        return RegistryKey.of(RegistryKeys.BLOCK, id);
     }
 
-    public static Block createBlock(Block block, String name, String MOD_ID){
-        createBlockItem(block, name, MOD_ID);
-        return Registry.register(Registries.BLOCK, Identifier.of(MOD_ID, name), block);
+    public static RegistryKey<Item> itemKeyOf(Identifier id) {
+        return RegistryKey.of(RegistryKeys.ITEM, id);
+    }
+
+    public static Block createBlock(Function<AbstractBlock.Settings, Block> factory, Identifier id, AbstractBlock.Settings settings) {
+        return register(keyOf(id), itemKeyOf(id), factory, settings);
+    }
+
+    public static Block register(RegistryKey<Block> blockKey, RegistryKey<Item> itemKey, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
+        Block block = factory.apply(settings.registryKey(blockKey));
+        Registry.register(Registries.ITEM, itemKey, new BlockItem(block, new Item.Settings().registryKey(itemKey)));
+        return Registry.register(Registries.BLOCK, blockKey, block);
     }
 }
--- src/main/java/ringed_strawberry/github/io/spacelib/item/ItemGen.java
+++ src/main/java/ringed_strawberry/github/io/spacelib/item/ItemGen.java
@@ -1,14 +1,25 @@
 package ringed_strawberry.github.io.spacelib.item;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
+
 public class ItemGen {
-    public static Item createItem(Item item, String name, String MOD_ID){
-        return Registry.register(Registries.ITEM, Identifier.of(MOD_ID, name), item);
+    public static Item createItem(Identifier id, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        return register(keyOf(id), factory, settings);
+    }
+
+    public static Item register(RegistryKey<Item> key, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        Item item = factory.apply(settings.registryKey(key));
+        return Registry.register(Registries.ITEM, key, item);
+    }
+
+    private static RegistryKey<Item> keyOf(Identifier id) {
+        return RegistryKey.of(RegistryKeys.ITEM, id);
     }
 }
