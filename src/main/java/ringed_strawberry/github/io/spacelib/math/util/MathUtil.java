a333d7f1352944c4419879defae9228616acee3c
saturn <thecolorizedsystem@gmail.com>
2025-10-20 20:58:26 +0300

    MORE MATHUTIL FIXES :SOB:

--- src/main/java/ringed_strawberry/github/io/spacelib/math/util/MathUtil.java
+++ src/main/java/ringed_strawberry/github/io/spacelib/math/util/MathUtil.java
@@ -6,10 +6,10 @@ public class MathUtil {
     }
 
     public static boolean isInRange(float input, float range, float radius){
-        return input <= range-radius && input >= range+radius;
+        return input >= range-radius && input <= range+radius;
     }
 
     public static boolean isInRange(float input, float range, int pixels){
-        return input <= range-pixels*0.07 && input >= range+pixels*0.07;
+        return input >= range-pixels*0.07 && input <= range+pixels*0.07;
     }
 }
