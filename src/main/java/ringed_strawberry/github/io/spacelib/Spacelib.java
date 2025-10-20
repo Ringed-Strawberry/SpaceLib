5f29b00793d6e926e8a16049048c1996982d4f65
saturn <thecolorizedsystem@gmail.com>
2025-10-20 20:13:22 +0300

    More BlockhitUtil

--- src/main/java/ringed_strawberry/github/io/spacelib/Spacelib.java
+++ src/main/java/ringed_strawberry/github/io/spacelib/Spacelib.java
@@ -1,11 +1,11 @@
 package ringed_strawberry.github.io.spacelib;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 
 public class Spacelib implements ModInitializer {
     public static final String MOD_ID = "spacelib";
     @Override
     public void onInitialize() {
-
     }
 }
--- src/main/java/ringed_strawberry/github/io/spacelib/block/util/BlockHitUtil.java
+++ src/main/java/ringed_strawberry/github/io/spacelib/block/util/BlockHitUtil.java
@@ -2,6 +2,7 @@ package ringed_strawberry.github.io.spacelib.block.util;
 
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec2f;
 import net.minecraft.util.math.Vec3d;
 
 public class BlockHitUtil {
@@ -11,5 +12,18 @@ public class BlockHitUtil {
 
     public static Vec3d getInteractAt(BlockPos bPos, Vec3d pos){
         return pos.add(bPos.getX()*-1, bPos.getY()*-1, bPos.getZ()*-1);
+    }
+
+    public static Vec2f getVec2fInteractAt(BlockHitResult hit){
+        float x = (float) (hit.getPos().getX() + hit.getBlockPos().getX()*-1);
+        float y = (float) (hit.getPos().getY() + hit.getBlockPos().getY()*-1);
+        float z = (float) (hit.getPos().getZ() + hit.getBlockPos().getZ()*-1);
+        if(z == 0 || z == 1)
+            return new Vec2f(x,y);
+        if(y == 0 || y == 1)
+            return new Vec2f(x,z);
+        if(x == 0 ||x == 1)
+            return new Vec2f(y,z);
+        return new Vec2f(x,y);
     }
 }
