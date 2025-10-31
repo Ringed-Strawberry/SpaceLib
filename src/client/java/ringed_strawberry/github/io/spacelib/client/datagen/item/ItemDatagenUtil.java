06ea6089458f54ff9b965847dbfafb4008a8498d
saturn <thecolorizedsystem@gmail.com>
2025-10-31 13:41:30 +0200

    Datagen extras extras

--- src/client/java/ringed_strawberry/github/io/spacelib/client/datagen/item/ItemDatagenUtil.java
+++ src/client/java/ringed_strawberry/github/io/spacelib/client/datagen/item/ItemDatagenUtil.java
@@ -17,11 +17,27 @@ public class ItemDatagenUtil {
         itemModelGenerator.output.accept(item, ItemModels.tinted(identifier, new TintSource[]{tint,tint1,tint2,tint3}));
     }
 
+    public final void registerWithFiveTints(ItemModelGenerator itemModelGenerator, Item item, TintSource tint, TintSource tint1, TintSource tint2, TintSource tint3, TintSource tint4){
+        Identifier identifier = uploadFiveLayers(item, TextureMap.getId(item),
+                TextureMap.getSubId(item, "_tinted1"),
+                TextureMap.getSubId(item, "_tinted2"),
+                TextureMap.getSubId(item, "_tinted3"),
+                TextureMap.getSubId(item, "_tinted4"),
+                itemModelGenerator);
+        itemModelGenerator.output.accept(item, ItemModels.tinted(identifier, new TintSource[]{tint,tint1,tint2,tint3,tint4}));
+    }
+
     public final Identifier uploadFourLayers(Item item, Identifier layer0, Identifier layer1, Identifier layer2, Identifier layer3, ItemModelGenerator itemModelGenerator) {
-        return GENERATED_FOUR_LAYERS.upload(item, layered(layer0, layer1, layer2, layer3), itemModelGenerator.modelCollector);
+        return GENERATED_FOUR_LAYERS.upload(item, layered4(layer0, layer1, layer2, layer3), itemModelGenerator.modelCollector);
+    }
+
+    public final Identifier uploadFiveLayers(Item item, Identifier layer0, Identifier layer1, Identifier layer2, Identifier layer3, Identifier layer4, ItemModelGenerator itemModelGenerator) {
+        return GENERATED_FIVE_LAYERS.upload(item, layered5(layer0, layer1, layer2, layer3, layer4), itemModelGenerator.modelCollector);
     }
     public static final TextureKey LAYER3 = of("layer3");
+    public static final TextureKey LAYER4 = of("layer4");
     public static final Model GENERATED_FOUR_LAYERS = item("generated", TextureKey.LAYER0, TextureKey.LAYER1, TextureKey.LAYER2, LAYER3);
+    public static final Model GENERATED_FIVE_LAYERS = item("generated", TextureKey.LAYER0, TextureKey.LAYER1, TextureKey.LAYER2, LAYER3, LAYER4);
 
     public static TextureKey of(String name) {
         return new TextureKey(name, null);
@@ -31,7 +47,11 @@ public class ItemDatagenUtil {
         return new Model(Optional.of(Identifier.ofVanilla("item/" + parent)), Optional.empty(), requiredTextureKeys);
     }
 
-    public static TextureMap layered(Identifier layer0, Identifier layer1, Identifier layer2, Identifier layer3) {
+    public static TextureMap layered4(Identifier layer0, Identifier layer1, Identifier layer2, Identifier layer3) {
         return (new TextureMap()).put(TextureKey.LAYER0, layer0).put(TextureKey.LAYER1, layer1).put(TextureKey.LAYER2, layer2).put(LAYER3, layer3);
+    }
+
+    public static TextureMap layered5(Identifier layer0, Identifier layer1, Identifier layer2, Identifier layer3, Identifier layer4) {
+        return (new TextureMap()).put(TextureKey.LAYER0, layer0).put(TextureKey.LAYER1, layer1).put(TextureKey.LAYER2, layer2).put(LAYER3, layer3).put(LAYER4, layer4);
     }
 }
