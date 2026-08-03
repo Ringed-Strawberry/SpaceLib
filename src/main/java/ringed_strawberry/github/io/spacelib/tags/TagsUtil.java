6469476d434fc65db398ad09ac9c419f7a82c515
rings-of-saturn <rings-of-saturn>
2026-08-03 14:13:00 +0300

    migrateMappings task

--- src/main/java/ringed_strawberry/github/io/spacelib/tags/TagsUtil.java
+++ src/main/java/ringed_strawberry/github/io/spacelib/tags/TagsUtil.java
@@ -1,8 +1,8 @@
 package ringed_strawberry.github.io.spacelib.tags;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.world.level.block.Block;
 
 public class TagsUtil {
     public static boolean isInTag(BlockState state, TagKey<Block> tag){
